428387c9cc492ff49d81a5429b08e0b3fccf4847
gtsiokos <gtsiokos@gmail.com>
2024-04-25 12:14:59 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Include products that are onbackorder when using the <a href="https://woocommerce.com/products/woocommerce-product-search/">WooCommerce Product Search</a>
---
+++ wps-include-backorder-products.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: WPS Include Backorder Products
+ * Plugin URI: https://github.com/geotsiokos/wps-include-backorder-products.git
+ * Description: Include products that are onbackorder when using the <a href="https://woocommerce.com/products/woocommerce-product-search/">WooCommerce Product Search</a>
+ * Version: 1.0.0
+ * Author: gtsiokos
+ * Author URI: http://www.netpad.gr
+ * Donate-Link: http://www.netpad.gr
+ * License: GPLv3
+ */
+
+if ( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+class Wps_Include_Backorder_Products {
+
+	public static function init() {
+		add_filter( 'woocommerce_product_search_engine_stage_parameters', array( __CLASS__, 'woocommerce_product_search_engine_stage_parameters' ), 10, 2 );
+	}
+
+	public static function woocommerce_product_search_engine_stage_parameters( $args, $stage ) {
+		error_log( 'arguments' );
+		error_log( print_r( $args, true ) );
+		$args['stock'] = 'instock, backorder';
+		return $args;
+	}
+} Wps_Include_Backorder_Products::init();
